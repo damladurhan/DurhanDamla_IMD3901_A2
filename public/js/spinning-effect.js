72f4d0d964af4a453f0e3ea686bd4212ceedd8d2
damladurhan <72809162+damladurhan@users.noreply.github.com>
2022-02-05 11:51:43 -0500

    refresh

--- public/js/spinning-effect.js
+++ public/js/spinning-effect.js
@@ -9,31 +9,29 @@ AFRAME.registerComponent('spinning-effect', {
         //setup. called once at creation after the aframe scene has loaded
 
         const CONTEXT_AF = this;
-        CONTEXT_AF.walls = document.querySelector('#walls');
-        //CONTEXT_AF.sculpture = document.querySelector('#sculpture');
+        CONTEXT_AF.sun = document.querySelector('#lightSphere');
         CONTEXT_AF.isSpinning = false;
 
-        CONTEXT_AF.walls.setAttribute('animation', {property:'position.x', from:-250, to:120, loop:true, dur:6000, easing:'linear', enabled:false});
+        CONTEXT_AF.sun.setAttribute('animation', {property:'position.x', from:-200, to:220, loop:true, dur:6000, easing:'linear', enabled:false});
         const ambientSounds = document.querySelectorAll('.ambient-sound');
         
 
         CONTEXT_AF.el.addEventListener('click', function(){
             if(CONTEXT_AF.isSpinning===true){
                 console.log('stop spinning');
-                CONTEXT_AF.walls.setAttribute('animation', {enabled:false});
+                CONTEXT_AF.sun.setAttribute('animation', {enabled:false});
                 CONTEXT_AF.isSpinning = false;  
-
-                
-              ambientSounds.forEach(function(soundEntity){
-                  soundEntity.components['sound'].playSound();
-              });
-                
+    
             }
             else{
                 console.log('start spinning');
-                CONTEXT_AF.walls.setAttribute('animation', {enabled:true});
+                CONTEXT_AF.sun.setAttribute('animation', {enabled:true});
 
                 CONTEXT_AF.isSpinning = true;
+
+                ambientSounds.forEach(function(soundEntity){
+                    soundEntity.components['sound'].playSound();
+                });
             }
         })
 
